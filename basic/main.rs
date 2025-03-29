85421aa6f0fe7d34b5411a662bbd16ca7a6d0bd5
Facundo Gonzalez <facujgg@gmail.com>
2025-03-29 17:22:31 -0300

    unit

--- basic/main.rs
+++ basic/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x:i32 = 10;
-    let y:i32 = 5;
-    let name: &str = "facu";
-    println!("My name is {} and the answer is {}", name, x + y)
+    let x:() = ();
+    let y:() = println!("wow");
+    assert_eq!(x, y);
+    println!("All units are the same")
 }
