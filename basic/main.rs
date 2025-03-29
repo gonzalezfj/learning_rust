e59e2379c1f1c727f6b14cbe8a224589e68f2752
Facundo Gonzalez <facujgg@gmail.com>
2025-03-29 17:15:51 -0300

    types

--- basic/main.rs
+++ basic/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let x = 10;
-    let y = 5;
-    let name = "facu";
+    let x:i32 = 10;
+    let y:i32 = 5;
+    let name: &str = "facu";
     println!("My name is {} and the answer is {}", name, x + y)
 }
